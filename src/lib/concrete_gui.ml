d4c45c4bf1d3033375aa54f04114a81ee98da4a2
Loussine <loussine.bas@etu.u-paris.fr>
2026-03-29 00:48:58 +0100

    update(concrete_gui): le rendu était indépendant du module wasm modification en conséquent avec l'ajout de init,draw_grid,draw_cell,...

--- src/lib/concrete_gui.ml
+++ src/lib/concrete_gui.ml
@@ -1,27 +1,60 @@
 open Raylib
 
-let numRows = 20
-let numCols = 20
+type d = {
+  mutable nb_rows : int;
+  mutable nb_cols : int;
+  cell_size : int;
+  mutable grid : int array array;
+  mutable initialised : bool;
+}
+
+let data:d = {
+  nb_rows = 0;
+  nb_cols = 0;
+  cell_size = 20;
+  grid = [||];
+  initialised = false;
+}
+
+let init ~nb_rows ~nb_cols =
+  data.nb_rows <- nb_rows;
+  data.nb_cols <- nb_cols;
+  data.grid <- Array.make_matrix nb_rows nb_cols 0;
+  let width = (nb_cols * data.cell_size) in
+  let height = (nb_rows * data.cell_size) in
+  init_window width height "Game of Life";
+  data.initialised <- true
+
+let set_cell ~row ~col ~alive =
+  if row >=0 && row < data.nb_rows && col>=0 && col < data.nb_cols then data.grid.(row).(col) <- if alive then 1 else 0
 
 let draw_grid ()=
-  let w = get_screen_width () in
-  let h = get_screen_height () in
-  let cellw = w/numCols in
-  let cellh = h/numRows in
-  List.init (numCols + 1) (fun i -> i * cellw)
-  |> List.iter (fun x -> draw_line x 0 x h Color.black);
-  List.init (numRows + 1) (fun j -> j * cellh)
-  |> List.iter (fun y -> draw_line 0 y w y Color.black)
+  let w = data.nb_cols * data.cell_size in
+  let h = data.nb_rows * data.cell_size in
+  let cols =  List.init (data.nb_cols + 1) (fun i -> i * data.cell_size) in
+  let rows = List.init (data.nb_rows + 1) (fun j -> j * data.cell_size) in
+  List.iter (fun x -> draw_line x 0 x h Color.black) cols;
+  List.iter (fun y -> draw_line 0 y w y Color.black) rows
 
-let run () = 
-  init_window 600 600 "test";
-  set_window_state [ConfigFlags.Window_resizable];
-  while not (window_should_close ()) do
-    begin_drawing ();
-    clear_background Color.raywhite;
-    draw_grid ();
-    draw_rectangle 0 0 10 10 Color.red;
-    end_drawing ();
-  done;
-  close_window();
-  Ok()
+let draw_cells ()=
+  Array.iteri (fun row_nb row_content ->
+    Array.iteri (fun col_nb cell_val ->
+      if cell_val <> 0 then
+        draw_rectangle (col_nb*data.cell_size) (row_nb*data.cell_size) (data.cell_size) (data.cell_size) Color.black 
+      ) row_content ) data.grid
+
+let render ()=
+  begin_drawing ();
+  clear_background Color.raywhite;
+  draw_grid ();
+  draw_cells ();
+  end_drawing ();
+
+let w_should_close () =
+  data.initialised && window_should_close ()
+
+let close () =
+  if data.initialised then begin
+    close_window ();
+    data.initialised <- false;
+  end
